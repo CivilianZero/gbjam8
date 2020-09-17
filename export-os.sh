1f660bc2964f96468568186d83b5ae6db8169113
civilianzero <civilianzero@icloud.com>
2020-09-17 14:08:15 -0400

    forgot to add the raspi build to the push, it's in there now

--- export-os.sh
+++ export-os.sh
@@ -10,5 +10,6 @@ $PICO8 -export slime_tactics.bin ./gbjam.p8
 butler push slime_tactics.bin/slime_tactics.app $username/slime-tactics:osx --if-changed
 butler push slime_tactics.bin/windows $username/slime-tactics:windows --if-changed
 butler push slime_tactics.bin/linux $username/slime-tactics:linux --if-changed
+butler push slime_tactics.bin/raspi $username/slime-tactics:raspi --if-changed
 
 rm -r slime_tactics.bin
