9b0660b73899167895b89ab2c9ac5ef701569cba
civilianzero <civilianzero@icloud.com>
2020-09-17 14:01:57 -0400

    still need butler installed as a global command (symlink or alias) but can now input itch.io username and if you have used butler to login previously it should work

--- export-html.sh
+++ export-html.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Input your itch.io username:"
+read username
+
 PICO8=/Applications/PICO-8.app/Contents/MacOS/pico8
 
 mkdir slime_tactics
@@ -7,7 +10,7 @@ $PICO8 -export slime_tactics/slime_tactics.html ./gbjam.p8
 mv slime_tactics/slime_tactics.html slime_tactics/index.html
 zip -r slime_tactics.zip slime_tactics
 
-butler push slime_tactics.zip civilianzero/slime_tactics:html --if-changed
+butler push slime_tactics.zip $username/slime-tactics:html --if-changed
 
-rm slime_tactics
+rm -r slime_tactics
 rm slime_tactics.zip
--- export-os.sh
+++ export-os.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo "Input your itch.io username"
+read username
+
 PICO8=/Applications/PICO-8.app/Contents/MacOS/pico8
 
 $PICO8 -export slime_tactics.bin ./gbjam.p8
 
-butler push slime_tactics.bin/slime_tactics_osx.zip civilianzero/slime-tactics:osx --if-changed
-butler push slime_tactics.bin/slime_tactics_windows.zip civilianzero/slime-tactics:windows --if-changed
-butler push slime_tactics.bin/slime_tactics_linux.zip civilianzero/slime-tactics:linux --if-changed
+butler push slime_tactics.bin/slime_tactics.app $username/slime-tactics:osx --if-changed
+butler push slime_tactics.bin/windows $username/slime-tactics:windows --if-changed
+butler push slime_tactics.bin/linux $username/slime-tactics:linux --if-changed
 
 rm -r slime_tactics.bin
